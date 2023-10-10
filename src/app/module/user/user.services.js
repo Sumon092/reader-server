502a662c46938d6800ef4eec5071a02e094ae14a
Sumon <m.sumon@outlook.com>
2023-10-10 18:46:29 +0600

    login error solved

--- src/app/module/user/user.services.js
+++ src/app/module/user/user.services.js
@@ -4,37 +4,58 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const createUser = async (userInfo) => {
-  const { email, password } = userInfo;
+  const { email, password, name, phoneNumber } = userInfo;
   const existingEmail = await User.findOne({ email: email });
   if (!existingEmail) {
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({
-      name: userInfo.name,
+      name: name,
       email: email,
-      phoneNumber: userInfo.phoneNumber,
+      phoneNumber: phoneNumber,
       password: hashedPassword,
     });
 
     await user.save();
     return user;
   } else {
-    throw new Error("User already exist");
+    throw new Error("User already exists");
   }
 };
+
+// const loginUser = async (email, password) => {
+//   const user = await User.findOne({ email });
+//   if (!user) {
+//     throw new Error("Invalid user or password");
+//   }
+
+//   const userEmail = user.email || "";
+//   const isPasswordValid = await bcrypt.compare(password, user.password);
+
+//   if (!isPasswordValid) {
+//     throw new Error("Invalid user or password");
+//   }
+
+//   const token = jwt.sign({ email: userEmail }, process.env.ACCESS_TOKEN_SECRET);
+//   return token;
+// };
+
 const loginUser = async (email, password) => {
-  const user = await User.findOne({ email });
-  if (!user) {
-    throw new Error("Invalid user or password");
+  try {
+    if (!email || !password) {
+      throw new Error("Invalid email or password");
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const token = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET);
+    return { token };
+  } catch (error) {
+    console.error("Login failed:", error);
+    throw error;
   }
-  const isPasswordValid = await bcrypt.compare(password, user.password);
-  if (!isPasswordValid) {
-    throw new Error("Invalid user or password");
-  }
-  const token = jwt.sign(
-    { email: user.email },
-    process.env.ACCESS_TOKEN_SECRET
-  );
-  return token;
 };
 
 const getAuthUser = async (email) => {
@@ -42,7 +63,7 @@ const getAuthUser = async (email) => {
   if (user) {
     return user;
   } else {
-    throw new Error("user is not authenticated");
+    throw new Error("User is not authenticated");
   }
 };
 
